270748872771273a8757ff6bcea7b0ce5329dbd6
sskates1 <simonirv2@gmail.com>
2014-06-06 15:33:14 -0400

    Added files to change it to a Active record instead of manual sql

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-contrib'
 
+gem 'sinatra-activerecord'
+gem 'pg'
+gem 'rake'
+
 group :test, :development do
   gem 'pry'
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,24 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/activerecord"
 
 require_relative 'models/contact'
 
-before do
-  contact_attributes = [
-    { first_name: 'Eric', last_name: 'Kelly', phone_number: '1234567890' },
-    { first_name: 'Adam', last_name: 'Sheehan', phone_number: '1234567890' },
-    { first_name: 'Dan', last_name: 'Pickett', phone_number: '1234567890' },
-    { first_name: 'Evan', last_name: 'Charles', phone_number: '1234567890' },
-    { first_name: 'Faizaan', last_name: 'Shamsi', phone_number: '1234567890' },
-    { first_name: 'Helen', last_name: 'Hood', phone_number: '1234567890' },
-    { first_name: 'Corinne', last_name: 'Babel', phone_number: '1234567890' }
-  ]
-
-  @contacts = contact_attributes.map do |attr|
-    Contact.new(attr)
-  end
+get '/' do
+  @contacts = Contact.all
+  erb :index
 end
 
-get '/' do
-  erb :index
+get '/contacts/:id' do
+  contact_id = params["id"]
+  @contact = Contact.find(contact_id)
+  erb :show
 end
